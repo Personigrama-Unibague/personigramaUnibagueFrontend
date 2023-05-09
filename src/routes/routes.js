0015215cfaa0251a8913890615db3f488c3e498d
JuanPabloMenesesBocanegra <juanweue@gmail.com>
2023-05-09 06:47:18 -0500

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,16 @@
-import  { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
-import HomePage from '../pages/HomePage';
-import AdminLogin from '../pages/AdminLogin/AdminLogin';
-import Personigrama from '../pages/Personigrama/Personigrama';
-import PrincipalTemporal from '../pages/PrincipalTemporal';
-import AdminUnidades from '../pages/AdminUnidades/AdminUnidades';
-import AdminUsuarios from '../pages/AdminUsuarios/AdminUsuarios';
-import ConfigurarSecciones from '../pages/ConfigurarSecciones/ConfigurarSecciones';
-import SeccionFuncionarios from '../pages/SeccionFuncionarios/SeccionFuncionarios';
-import NotFound from '../pages/NotFound/NotFound';
-import PruebasJson from '../pages/PruebasJson';
-import PruebaLogin from '../pages/PruebaLogIn/PruebaLogIn'
+import HomePage from "../pages/HomePage";
+import AdminLogin from "../pages/AdminLogin/AdminLogin";
+import Personigrama from "../pages/Personigrama/Personigrama";
+import PrincipalTemporal from "../pages/PrincipalTemporal";
+import AdminUnidades from "../pages/AdminUnidades/AdminUnidades";
+import AdminUsuarios from "../pages/AdminUsuarios/AdminUsuarios";
+import ConfigurarSecciones from "../pages/ConfigurarSecciones/ConfigurarSecciones";
+import SeccionFuncionarios from "../pages/SeccionFuncionarios/SeccionFuncionarios";
+import NotFound from "../pages/NotFound/NotFound";
+import PruebasJson from "../pages/PruebasJson";
+import PruebaLogin from "../pages/PruebaLogIn/PruebaLogIn";
 
 const routes = () => {
   return (
@@ -23,8 +23,14 @@ const routes = () => {
           <Route path="adminLogin" element={<AdminLogin />}></Route>
           <Route path="adminUnidades" element={<AdminUnidades />}></Route>
           <Route path="adminUsuarios" element={<AdminUsuarios />}></Route>
-          <Route path="confiSecciones" element={<ConfigurarSecciones />}></Route>
-          <Route path="seccionFuncionarios" element={<SeccionFuncionarios />}></Route>
+          <Route
+            path="confiSecciones"
+            element={<ConfigurarSecciones />}
+          ></Route>
+          <Route
+            path="seccionFuncionarios"
+            element={<SeccionFuncionarios />}
+          ></Route>
           <Route path="pruebasjson" element={<PruebasJson />}></Route>
           <Route path="pruebaLogin" element={<PruebaLogin />}></Route>
           <Route path="*" element={<NotFound />}></Route>
