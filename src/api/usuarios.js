4236914b42dcfd023473dc25f993e94037986484
Juan Esteban Gomez Roa <jgomezr@famisanar.com.co>
2023-05-30 22:14:03 -0500

    7,8,9

--- src/api/usuarios.js
+++ src/api/usuarios.js
@@ -13,3 +13,27 @@ export const getSaveNewUser = async (user, password) => {
     return [];
   }
 };
+
+export const getPersonal = async () => {
+  try {
+    const response = await axios.get(
+      `http://localhost:9090/api/v1/login/findAllUsers`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+export const getDeleteUser = async (id) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:9090/api/v1/login/deleteUser/${id}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
--- src/pages/AdminUsuarios/AdminUsuarios.js
+++ src/pages/AdminUsuarios/AdminUsuarios.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { Component, useState, useEffect, useLayoutEffect } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -11,6 +11,7 @@ import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
+import HttpsIcon from "@mui/icons-material/Https";
 import TableHead from "@mui/material/TableHead";
 import CloseIcon from "@mui/icons-material/Close";
 import TableRow from "@mui/material/TableRow";
@@ -25,159 +26,262 @@ import TextField from "material-ui/TextField";
 import "./styles.css";
 import { Dialog, ListItem } from "material-ui";
 import List from "@mui/material/List";
-
-function createData(name, calories, fat) {
-  return { name, calories, fat };
-}
-
-const rows = [
-  createData(
-    "Dirección y Planeación",
-    "direccdeplaneacion@unibague.edu.co",
-    6.0,
-    24,
-    4.0
-  ),
-  createData(
-    "Maria Villamizar",
-    "maria.villamizar@unibague.edu.co",
-    2,
-    37,
-    4.3
-  ),
-  createData(
-    "Julian Hernandez",
-    "julian.hernandez@eunibague.edu.co  ",
-    16.0,
-    24,
-    6.0
-  ),
-  createData(
-    "Comunicación institucional",
-    "comuinstitucional@unibague.edu.co",
-    3.7,
-    67,
-    4.3
-  ),
-];
+import { getDeleteUser, getPersonal, getSaveNewUser } from "../../api/usuarios";
 
 function AdminUsuarios() {
   const [open, setOpen] = React.useState(false);
+
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  return (
-    <Grid
-      container
-      component="main"
-      style={{ justifyContent: "center", display: "flex" }}
-    >
-      <Grid xs={12}>
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="static">
-            <Toolbar style={{ backgroundColor: "#193F76" }}>
-              <Typography
-                variant="h6"
-                component="div"
-                sx={{ flexGrow: 1 }}
-                style={{
-                  justifyContent: "center",
-                  display: "flex",
-                  font: "Lato",
-                  fontSize: "15px",
-                }}
-              >
-                Administar Usuarios
-              </Typography>
-              <InputOutlinedIcon
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: 3 }}
-              >
-                <MenuIcon />
-              </InputOutlinedIcon>
-            </Toolbar>
-          </AppBar>
-        </Box>
-      </Grid>
+  const [userList, setUserList] = React.useState([]);
+  const [user, setUser] = React.useState([]);
+  const [password, setPassword] = React.useState([]);
 
-      {/* Grid Table */}
-      <Grid item style={{ marginTop: "50px" }}>
-        {/* Table */}
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell
-                  className="tableCellTitle"
-                  style={{ backgroundColor: "#017A97", color: "white" }}
+  useLayoutEffect(() => {
+    (async () => {
+      try {
+        const list = await getPersonal();
+        setUserList(list);
+      } catch (err) {
+        console.log("Error API");
+      }
+    })();
+  }, []);
+
+  const onChangeUser = (event) => {
+    setUser(event.target.value);
+  };
+
+  const onChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const saveUser = () => {
+    getSaveNewUser(user, password);
+    setTimeout(window.location.reload(), 10000);
+  };
+
+  const deleteUser = (id) => {
+    getDeleteUser(id);
+    setTimeout(window.location.reload(), 10000);
+  };
+  return (
+    <>
+      <Grid
+        container
+        component="main"
+        style={{ justifyContent: "center", display: "flex" }}
+        justify="center"
+      >
+        <Grid xs={12}>
+          <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+              <Toolbar style={{ backgroundColor: "#193F76" }}>
+                <Typography
+                  variant="h6"
+                  component="div"
+                  sx={{ flexGrow: 1 }}
+                  style={{
+                    justifyContent: "center",
+                    display: "flex",
+                    font: "Lato",
+                    fontSize: "15px",
+                  }}
                 >
-                  Usuario Registrados
-                </TableCell>
-                <TableCell
-                  className="tableCellTitle"
-                  align="center"
-                  style={{ backgroundColor: "#017A97", color: "white" }}
+                  Administar Usuarios
+                </Typography>
+                <InputOutlinedIcon
+                  size="large"
+                  edge="start"
+                  color="inherit"
+                  aria-label="menu"
+                  sx={{ mr: 3 }}
                 >
-                  Correo
-                </TableCell>
-                <TableCell
-                  className="tableCellTitle"
-                  align="center"
-                  style={{ backgroundColor: "#017A97", color: "white" }}
-                >
-                  Opciones
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => (
-                <TableRow key={row.name}>
-                  <TableCell component="th" scope="row" className="tableCell">
-                    {row.name}
+                  <MenuIcon />
+                </InputOutlinedIcon>
+              </Toolbar>
+            </AppBar>
+          </Box>
+        </Grid>
+
+        {/* Grid Table */}
+        <Grid item style={{ marginTop: "50px" }}>
+          {/* Table */}
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell
+                    className="tableCellTitle"
+                    align="center"
+                    style={{ backgroundColor: "#017A97", color: "white" }}
+                  >
+                    Usuario
                   </TableCell>
-                  <TableCell align="center" className="tableCell">
-                    {row.calories}{" "}
-                  </TableCell>
-                  <TableCell align="center">
-                    <IconButton
-                      className="IconButton"
-                      variant="outlined"
-                      style={{
-                        backgroundColor: "#B8B9BA",
-                        borderRadius: "10px",
-                        color: "white",
-                      }}
-                    >
-                      <BorderColorOutlinedIcon
-                        className="icon"
-                        style={{ color: "white" }}
-                      />
-                    </IconButton>
-                    <IconButton
-                      className="IconButton"
-                      variant="outlined"
-                      style={{
-                        backgroundColor: "#B8B9BA",
-                        borderRadius: "10px",
-                        color: "white",
-                      }}
-                    >
-                      <DeleteOutlineOutlinedIcon
-                        className="icon"
-                        style={{ color: "white" }}
-                      />
-                    </IconButton>
+                  <TableCell
+                    className="tableCellTitle"
+                    align="center"
+                    style={{ backgroundColor: "#017A97", color: "white" }}
+                  >
+                    Opciones
                   </TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {userList.map((user) => (
+                  <TableRow key={user.id} className="tableCell">
+                    <TableCell
+                      align="center"
+                      component="th"
+                      scope="row"
+                      className="tableCell"
+                    >
+                      {user.usuario}
+                    </TableCell>
+                    <TableCell align="center">
+                      <IconButton
+                        className="IconButton"
+                        variant="outlined"
+                        onClick={() => deleteUser(user.id)}
+                        style={{
+                          backgroundColor: "#B8B9BA",
+                          borderRadius: "10px",
+                          color: "white",
+                        }}
+                      >
+                        <DeleteOutlineOutlinedIcon
+                          className="icon"
+                          style={{ color: "white" }}
+                        />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+
+          {/*Modal Agregar usuario */}
+          <Dialog open={open} onClose={handleClose}>
+            <List sx={{ pt: 0 }}>
+              <Toolbar className="modalTitle">
+                <Typography
+                  className="typpgraphyTitle"
+                  variant="h5"
+                  style={{
+                    flexGrow: 1,
+                    textAlign: "center",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Agregar Usuario
+                </Typography>
+                <IconButton
+                  edge="end"
+                  className="IconButton"
+                  variant="outlined"
+                  style={{
+                    color: "white",
+                  }}
+                  onClick={() => handleClose()}
+                >
+                  <CloseIcon className="icon" style={{ color: "white" }} />
+                </IconButton>
+              </Toolbar>
+
+              <ListItem>
+                <Typography variant="subtitle1" style={{ paddingLeft: "20px" }}>
+                  Usuario
+                </Typography>
+              </ListItem>
+
+              <ListItem style={{ paddingTop: "5px", width: "450px" }}>
+                <TextField
+                  className="textField"
+                  placeholder="Usuario"
+                  value={user}
+                  onChange={onChangeUser}
+                  style={{
+                    backgroundColor: "#FFFFFF",
+                    borderRadius: "30px",
+                    borderColor: "#04B8E2",
+                  }}
+                  InputProps={{
+                    startAdornment: (
+                      <IconButton
+                        color="white"
+                        sx={{ p: "10px" }}
+                        style={{
+                          borderRadius: "30px 0px 0px 30px",
+                          backgroundColor: "#04B8E2",
+                        }}
+                        position="start"
+                      >
+                        <EmailIcon style={{ color: "white" }} />
+                      </IconButton>
+                    ),
+                  }}
+                />
+              </ListItem>
+
+              <ListItem>
+                <Typography variant="subtitle1" style={{ paddingLeft: "20px" }}>
+                  Contraseña
+                </Typography>
+              </ListItem>
+
+              <ListItem style={{ paddingTop: "5px", width: "450px" }}>
+                <TextField
+                  className="textField"
+                  placeholder="Contraseña"
+                  value={password}
+                  onChange={onChangePassword}
+                  style={{
+                    backgroundColor: "#FFFFFF",
+                    borderRadius: "30px",
+                    borderColor: "#04B8E2",
+                  }}
+                  InputProps={{
+                    startAdornment: (
+                      <IconButton
+                        color="white"
+                        sx={{ p: "10px" }}
+                        style={{
+                          borderRadius: "30px 0px 0px 30px",
+                          backgroundColor: "#04B8E2",
+                        }}
+                        position="start"
+                      >
+                        <HttpsIcon style={{ color: "white" }} />
+                      </IconButton>
+                    ),
+                  }}
+                />
+              </ListItem>
+
+              <ListItem style={{ display: "flex", justifyContent: "center" }}>
+                <Button
+                  className="agregarUsuarioButton"
+                  variant="outlined"
+                  onClick={saveUser}
+                  style={{
+                    backgroundColor: "#04B8E2",
+                    color: "white",
+                    marginTop: "30px",
+                    borderRadius: "50px",
+                  }}
+                >
+                  Agregar
+                </Button>
+              </ListItem>
+            </List>
+          </Dialog>
+        </Grid>
+      </Grid>
+
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <Button
-          className="agregarUsuarioButton"
           variant="outlined"
           onClick={handleOpen}
           startIcon={<PersonAddAltOutlinedIcon />}
@@ -185,124 +289,13 @@ function AdminUsuarios() {
             backgroundColor: "#04B8E2",
             color: "white",
             marginTop: "30px",
-            marginLeft: "10vw",
             borderRadius: "50px",
           }}
         >
           Agregar Usuario
         </Button>
-
-        {/*Modal Agregar usuario */}
-        <Dialog open={open} onClose={handleClose}>
-          <List sx={{ pt: 0 }}>
-            <Toolbar className="modalTitle">
-              <Typography
-                className="typpgraphyTitle"
-                variant="h5"
-                style={{ flexGrow: 1, textAlign: "center", fontWeight: "bold" }}
-              >
-                Agregar Usuario
-              </Typography>
-              <IconButton
-                edge="end"
-                className="IconButton"
-                variant="outlined"
-                style={{
-                  color: "white",
-                }}
-                onClick={() => handleClose()}
-              >
-                <CloseIcon className="icon" style={{ color: "white" }} />
-              </IconButton>
-            </Toolbar>
-
-            <ListItem>
-              <Typography variant="subtitle1" style={{ paddingLeft: "20px" }}>
-                Usuario
-              </Typography>
-            </ListItem>
-
-            <ListItem style={{ paddingTop: "5px", width: "450px" }}>
-              <TextField
-                className="textField"
-                placeholder="Usuario"
-                focused
-                style={{
-                  backgroundColor: "#FFFFFF",
-                  borderRadius: "30px",
-                  borderColor: "#04B8E2",
-                }}
-                InputProps={{
-                  startAdornment: (
-                    <IconButton
-                      color="white"
-                      sx={{ p: "10px" }}
-                      style={{
-                        borderRadius: "30px 0px 0px 30px",
-                        backgroundColor: "#04B8E2",
-                      }}
-                      position="start"
-                    >
-                      <EmailIcon style={{ color: "white" }} />
-                    </IconButton>
-                  ),
-                }}
-              />
-            </ListItem>
-
-            <ListItem>
-              <Typography variant="subtitle1" style={{ paddingLeft: "20px" }}>
-                Contraseña
-              </Typography>
-            </ListItem>
-
-            <ListItem style={{ paddingTop: "5px", width: "450px" }}>
-              <TextField
-                className="textField"
-                placeholder="Contraseña"
-                focused
-                style={{
-                  backgroundColor: "#FFFFFF",
-                  borderRadius: "30px",
-                  borderColor: "#04B8E2",
-                }}
-                InputProps={{
-                  startAdornment: (
-                    <IconButton
-                      color="white"
-                      sx={{ p: "10px" }}
-                      style={{
-                        borderRadius: "30px 0px 0px 30px",
-                        backgroundColor: "#04B8E2",
-                      }}
-                      position="start"
-                    >
-                      <EmailIcon style={{ color: "white" }} />
-                    </IconButton>
-                  ),
-                }}
-              />
-            </ListItem>
-            
-            <ListItem style={{ display: "flex", justifyContent: "center" }}>
-              <Button
-                className="agregarUsuarioButton"
-                variant="outlined"
-                onClick={handleOpen}
-                style={{
-                  backgroundColor: "#04B8E2",
-                  color: "white",
-                  marginTop: "30px",
-                  borderRadius: "50px",
-                }}
-              >
-                Agregar
-              </Button>
-            </ListItem>
-          </List>
-        </Dialog>
-      </Grid>
-    </Grid>
+      </div>
+    </>
   );
 }
 
