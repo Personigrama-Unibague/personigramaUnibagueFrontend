0a649f931657032f55725b37c66dd97250ff3a8a
JuanPabloMenesesBocanegra <juanweue@gmail.com>
2023-08-20 18:28:59 -0500

    .

--- src/pages/Organigrama/Organigrama.js
+++ src/pages/Organigrama/Organigrama.js
@@ -71,37 +71,35 @@ export default function Organigrama() {
               onMouseOut={(event) => {
                 event.target.style.fill = "#02afd8"; // Restaurar el color original al salir del mouse
                 event.target.nextSibling.style.fill = "black";
-                event.target.nextSibling.style.stroke = "black";
+                event.target.nextSibling.style.stroke = "none";
               }}
             />
-
-            <foreignObject x="0" y="0" width="350" height="85">
-              <div
-                xmlns="http://www.w3.org/1999/xhtml"
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  overflow: "hidden",
-                }}
-              >
-                {nodeDatum.nombre !== undefined &&
-                  nodeDatum.nombre !== "" &&
-                  nodeDatum.name}
-                {nodeDatum.nombre !== "" && nodeDatum.nombre}
-              </div>
-            </foreignObject>
+            <text
+              x="35%"
+              y="50%"
+              fill="black"
+              stroke="none"
+              font-size="13"
+              font-weight="180"
+              text-anchor="middle"
+              alignment-baseline="middle"
+              textWrap="20"
+              whiteSpace="pre-wrap"
+            >
+              {nodeDatum.nombre !== undefined &&
+                nodeDatum.nombre !== "" &&
+                nodeDatum.name}
+              {nodeDatum.nombre !== "" && nodeDatum.nombre}
+            </text>
 
             <a href={`/personigrama/${nodeDatum.id}/${nodeDatum.nombre}`}>
               <g transform="translate(350, 32.5)">
                 <svg
                   viewBox="0 0 640 512"
                   fill="#FFFFFF"
-                  height="1em"
-                  width="1em"
-                  x="0"
+                  height="1.5em"
+                  width="1.5em"
+                  x="-30"
                   y="0"
                 >
                   <path d="M144 160c-44.2 0-80-35.8-80-80S99.8 0 144 0s80 35.8 80 80-35.8 80-80 80zm368 0c-44.2 0-80-35.8-80-80s35.8-80 80-80 80 35.8 80 80-35.8 80-80 80zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7-1.3 7.2-1.9 14.7-1.9 22.3 0 38.2 16.8 72.5 43.3 96H21.3C9.6 320 0 310.4 0 298.7zM405.3 320h-.7c26.6-23.5 43.3-57.8 43.3-96 0-7.6-.7-15-1.9-22.3 13.6-6.3 28.7-9.7 44.6-9.7h42.7c58.9 0 106.7 47.8 106.7 106.7 0 11.8-9.6 21.3-21.3 21.3H405.3zm10.7-96c0 53-43 96-96 96s-96-43-96-96 43-96 96-96 96 43 96 96zM128 485.3c0-73.6 59.7-133.3 133.3-133.3h117.4c73.6 0 133.3 59.7 133.3 133.3 0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z" />
@@ -118,9 +116,9 @@ export default function Organigrama() {
                   strokeWidth={2}
                   viewBox="0 0 24 24"
                   fill="none"
-                  height="1em"
-                  width="2em"
-                  x="0"
+                  height="1.5em"
+                  width="1.5em"
+                  x="-10"
                   y="0"
                   onClick={toggleNode}
                 >
@@ -168,7 +166,7 @@ export default function Organigrama() {
               x="50%"
               y="50%"
               fill="#FFFFFF"
-              stroke="#FFFFFF"
+              stroke="none"
               font-size="15"
               font-weight="200"
               text-anchor="middle"
