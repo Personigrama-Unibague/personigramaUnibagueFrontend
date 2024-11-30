546ceca2e270362e3698fff0dca8601e7f686b3a
JEstebanGnz <juanes01.gm@outlook.com>
2024-11-29 19:48:25 -0500

    Favicon changed and app title updated

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 //Clase para variables de configuracion
 
-export const API_URL = "http://personigrama.unibague.edu.co:9090/personigramaunibagueBackend";
+export const API_URL = "https://personigrama.unibague.edu.co:9090/personigramaunibagueBackend";
--- src/routes/GuardedRoute.js
+++ src/routes/GuardedRoute.js
@@ -1,14 +1,14 @@
 import React from "react";
-import jwt_decode from "jwt-decode";
 import { Route, Navigate, Routes } from "react-router-dom";
 import Cookies from "js-cookie";
+import { jwtDecode } from "jwt-decode";
 
 const GuardedRoute = ({
-  path,
-  element: Element,
-  protected: isProtected,
-  ...props
-}) => {
+                        path,
+                        element: Element,
+                        protected: isProtected,
+                        ...props
+                      }) => {
   const loginTime = Cookies.get("loginTime");
   const twentyFourHours = 24 * 60 * 60 * 1000;
   const currentTime = new Date().getTime();
@@ -23,22 +23,20 @@ const GuardedRoute = ({
   const user = Cookies.get("username");
 
   if (jwt) {
-    const decodedToken = jwt_decode(jwt);
+    const decodedToken = jwtDecode(jwt); // Updated method call
     const jwtUser = decodedToken.sub;
 
-    if (user == jwtUser) {
+    if (user === jwtUser) {
       return (
-        // Si el usuario está autenticado, redirige a la ruta especificada
-        <Routes>
-          <Route path="/" element={Element} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={Element} />
+          </Routes>
       );
     } else {
       window.alert("Error Autenticación");
       return <Navigate to="/adminLogin" replace />;
     }
   } else {
-    // Si el usuario no está autenticado, redirige a la página de inicio de sesión
     window.alert("Debes loguearte para acceder a este link");
     return <Navigate to="/adminLogin" replace />;
   }
